0b7e2b910ad6e8a3f65f91b9166170ff3041313b
Danyl Timofeyev <timofeev.danil@gmail.com>
2021-03-14 16:11:46 +0200

    Add dummy demo

--- ClaweeUI/MainModule/ViewController.swift
+++ ClaweeUI/MainModule/ViewController.swift
@@ -19,35 +19,7 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        let today: Date = Date()
-        let nextDate: Date = Calendar.current.date(byAdding: .day, value: 3, to: today)!
-
-        let heartProgressState = PlaySagaButton.HeartProgressState(progress: 0.2, heartsCount: 3)
-        let timerState = PlaySagaButton.TimerState(timerExpirationDate: nextDate) {
-            Logger.log("Timer time has gone", type: .all)
-        }
-        
-        let initialState = PlaySagaButton.InitialState(isTopTitleHidden: false,
-                                                   timerState: timerState,
-                                                   heartProgressState: heartProgressState)
-        
-        sagaButton.update(with: PlaySagaButton.State.initial(initialState))
-        
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-            let heartProgressState = PlaySagaButton.HeartProgressState(progress: 0.7, heartsCount: 6)
-            self.sagaButton.update(with: PlaySagaButton.State.heartProgress(heartProgressState))
-        }
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 6) {
-            let heartProgressState = PlaySagaButton.HeartProgressState(progress: 0.2, heartsCount: 7)
-            self.sagaButton.update(with: PlaySagaButton.State.heartProgress(heartProgressState))
-        }
-
-        DispatchQueue.main.asyncAfter(deadline: .now() + 8) {
-            let heartProgressState = PlaySagaButton.HeartProgressState(progress: 0.5, heartsCount: 8)
-            self.sagaButton.update(with: PlaySagaButton.State.heartProgress(heartProgressState))
-        }
+        sagaButton.dummyCurrentXibDemo()
     }
 
 }
--- ClaweeUI/PlaySagaButton/PlaySagaButton.swift
+++ ClaweeUI/PlaySagaButton/PlaySagaButton.swift
@@ -95,6 +95,39 @@ final class PlaySagaButton: TouchScaleButton {
     deinit {
         timer.stopTimer()
     }
+    
+    #warning("This is for presentation purpose. Delete it later")
+    public func dummyCurrentXibDemo() {
+        let today: Date = Date()
+        let nextDate: Date = Calendar.current.date(byAdding: .day, value: 3, to: today)!
+        
+        let heartProgressState = PlaySagaButton.HeartProgressState(progress: 0.2, heartsCount: 3)
+        let timerState = PlaySagaButton.TimerState(timerExpirationDate: nextDate) {
+            Logger.log("Timer time has gone", type: .all)
+        }
+        
+        let initialState = PlaySagaButton.InitialState(isTopTitleHidden: false,
+                                                       timerState: timerState,
+                                                       heartProgressState: heartProgressState)
+        
+        self.update(with: PlaySagaButton.State.initial(initialState))
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            let heartProgressState = PlaySagaButton.HeartProgressState(progress: 0.7, heartsCount: 6)
+            self.update(with: PlaySagaButton.State.heartProgress(heartProgressState))
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 6) {
+            let heartProgressState = PlaySagaButton.HeartProgressState(progress: 0.2, heartsCount: 7)
+            self.update(with: PlaySagaButton.State.heartProgress(heartProgressState))
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 8) {
+            let heartProgressState = PlaySagaButton.HeartProgressState(progress: 0.5, heartsCount: 8)
+            self.update(with: PlaySagaButton.State.heartProgress(heartProgressState))
+        }
+    }
 }
 
 
